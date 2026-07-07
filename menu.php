a14d63843b8a05a367e6dd5d90d45714942b5718
Khaos <ktree195@gmail.com>
2026-07-07 21:35:32 +0700

    lol

--- menu.php
+++ menu.php
@@ -2,17 +2,18 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-$username = isset($_SESSION['username']) && $_SESSION['username'] !== '' ? $_SESSION['username'] : 'Login';
+$username = !empty($_SESSION['username']) ? $_SESSION['username'] : 'Login';
 ?>
 
 <!-- The Side Menu -->
 <button id="sidebarToggle" class="sidebar-toggle" aria-expanded="true" aria-controls="sidebar">☰ Menu</button>
 <div class="sidebar" id="sidebar">
+    <a href="profile.php" class="profile-link">
+        <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Crect width='64' height='64' rx='32' fill='%234563ea'/%3E%3Ccircle cx='32' cy='24' r='14' fill='%23ffffff'/%3E%3Cpath d='M18 50c3-10 11-15 14-15s11 5 14 15' fill='%23ffffff'/%3E%3C/svg%3E" alt="Profile" class="profile-avatar">
+        <span><?php echo htmlspecialchars($username); ?></span>
+    </a><br>
     <a href="Artwork.php">Gallery</a><br>
     <a href="imageform.php">Insert Image</a><br>
-    <a href="profile.php">
-        <?php echo htmlspecialchars($username); ?>
-    </a><br>
     <a href="logout.php">Logout</a><br>
 </div>
 <script>
