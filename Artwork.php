7884c79df43acd743772ab25bda0bbb695ec7430
Khaos <007009ix@gmail.com>
2026-07-08 09:08:47 +0700

    Use local DB connection in Artwork.php; ensure images link to veiw_art.php and close connection

--- Artwork.php
+++ Artwork.php
@@ -10,34 +10,36 @@
 
     </head>
     <body>
-        
         <?php
-            $conn = mysqli_connect("localhost", "root", "", "ArtShopDB",3306);
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
+            // Use a local connection variable to avoid interfering with other includes
+            $artConn = new mysqli("localhost", "root", "", "ArtShopDB", 3306);
+            if ($artConn->connect_error) {
+                die("Connection failed: " . $artConn->connect_error);
             }
+
             $sql = "SELECT * FROM Artdata";
-            $result = mysqli_query($conn, $sql);
+            $result = $artConn->query($sql);
             if (!$result) {
-                die("Query failed: " . mysqli_error($conn));
+                die("Query failed: " . $artConn->error);
             }
         ?>
-    <div class=" py-1">
+    <div class="py-1">
             <?php include 'banner.php'; ?>
             <?php include 'menu.php';?>
     </div>
     <div class="main-content">
     <div class="container mt-4">
     <div class="row g-4">
-        <?php while($row = mysqli_fetch_assoc($result)) { ?>
+        <?php while($row = $result->fetch_assoc()) { ?>
         <div class="col-md-4">
             <div class="gallery h-100">
-                <a href="veiw_art.php?id=<?php echo htmlspecialchars($row['ArtID']); ?>">
+                <a href="veiw_art.php?id=<?php echo urlencode($row['ArtID']); ?>">
                     <img 
                     src="images/<?php echo htmlspecialchars($row['image_name']); ?>" 
-                    class="card-img-top gallery-img"
+                    class="card-img-top gallery-img img-fluid"
                     alt="<?php echo htmlspecialchars($row['ArtName']); ?>">
                 </a>
+                <div class="mt-2 text-center small text-muted"><?php echo htmlspecialchars($row['ArtName']); ?></div>
             </div>
         </div>
 
@@ -47,5 +49,12 @@
 
 </div>
     </div>
+    <?php
+        // free result and close local connection
+        if (isset($result) && $result instanceof mysqli_result) {
+            $result->free();
+        }
+        $artConn->close();
+    ?>
     </body>
 </html>
